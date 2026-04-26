babea674a74aed151d95cc2dc82c98d0585404ca
adamz <adam@zethrae.us>
2026-04-26 04:00:11 -0700

    remove legacy surfaces

--- GUI/XtermWebTUIGUI/src/vendor/xterm.ts
+++
@@ -1,5 +0,0 @@
-export { FitAddon } from "@xterm/addon-fit";
-export { Terminal } from "@xterm/xterm";
-export type { ITheme, ITerminalOptions } from "@xterm/xterm";
-
-export async function init(): Promise<void> {}
